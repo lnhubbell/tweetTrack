ad5bf79f002524515056e73ee609b6e88c144dd2
Ian Auld <imauld@gmail.com>
2014-07-17 19:58:30 -0700

    Added Tweet model

--- tweetTrack/app/__init__.py
+++ tweetTrack/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from tweetTrack.app import views, models
+from tweetTrack.app import views
