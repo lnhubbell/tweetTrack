79b750547dc7986710d96b75d274dec9dbc9fcfa
corinnelhh <corinne.hutchinson@gmail.com>
2014-07-18 15:00:50 -0700

    uploaded changes

--- twitter_stream.py
+++ twitter_stream.py
@@ -1,4 +1,3 @@
-import pprint
 import time
 import json
 from tweepy.streaming import StreamListener
@@ -23,57 +22,62 @@ class StdOutListener(StreamListener):
         self.data_count = 0
         self.location_count = 0
         self.seattle_count = 0
+        self.count = 0
+        self.start_time = time.clock()
 
     def on_data(self, data):
 
         json_data = json.loads(data)
-
-        tweet_id = json_data.get('id', None)
         text = json_data.get('text', None)
         try:
             hashtags = [i['text'] for i in json_data.get('entities', None).get('hashtags', None)]
         except AttributeError:
             print "I HAD THIS ERROR"
             return
-        user_mentions = [i['screen_name'] for i in json_data.get('entities', None).get('user_mentions', None)]
         created_at = json_data.get('created_at', None)
         screen_name = json_data.get('user', None).get('screen_name', None)
-        url = [i['display_url'] for i in json_data.get('entities', None).get('urls', None)]
         location = json_data.get('geo', None)
         if location:
-            location = location.get('coordinates', None)
+            _location = location.get('coordinates', None)
             self.location_count += 1
-        in_reply_to_screen_name = json_data.get('in_reply_to_screen_name', None)
-        retweets = json_data.get('retweet_count', None)
+        language = json_data.get('lang', None)
 
-        if hashtags:
-            self.hashtags_count += 1
-        if 'Seattle' in text or 'Seattle' in hashtags:
-            self.seattle_count += 1
-        self.data_count += 1
+        place = json_data.get('place', None)
+        if place:
+            country_code = place.get('country_code', None)
 
-        if location:
-            print json_data
-            print "Twitter Name: ", screen_name, type(screen_name)
-            # print "Tweet ID: ", tweet_id, type(tweet_id)
-            print "Hashtags: ", hashtags, type(hashtags)
-            print "URLs :", url, type(url)
-            print "Text: ", text, type(text)
-            # print "retweets: ", retweets, type(retweets)
-            print "User mentions: ", user_mentions, type(user_mentions)
-            # print "Created at: ", created_at, type(created_at)
-            print "Location: ", location, type(location)
-            print "In reply to: ", in_reply_to_screen_name, type(in_reply_to_screen_name)
-            # print '*'*20
-            # print "Data count: ", self.data_count
-            # print "Hashtag count: ", self.hashtags_count
-            # print "Location count: ", self.location_count
-            # print "Seattle Count: ", self.seattle_count
+        if location and (language == 'en'):
+            self.count += 1
+            end_time = time.clock()
+            #print json_data
+            print "Twitter Name: ", screen_name
+            print "Language: ", language
+            print "Hashtags: ", hashtags
+            print "Text: ", text
+            print "Time: ", created_at
+            print "Location: ", _location
+            print "Country code: ", country_code
+
             with open("output.txt", "a") as myfile:
-                myfile.write("Twitter Name:")
-                myfile.write(screen_name)
-                myfile.write("\n")
-            print "*"*20
+                myfile.write("Twitter Name: {}\n".format(screen_name))
+                # myfile.write("Language: {}\n".format(language))
+                # myfile.write("Hashtags: {}\n".format(hashtags))
+                # myfile.write("Text: {}\n".format(text))
+                # myfile.write("Time: {}\n".format(created_at))
+                # myfile.write("Time Zone: {}\n".format(t_zone))
+                # myfile.write("Place name: {}\n".format(place_name))
+                # myfile.write("Country code: {}\n".format(country_code))
+                # myfile.write("Bounding box: {}\n".format(bounding_box))
+                # myfile.write("***************************************************\n")
+            print "*" * 20
+            #time_elapsed = (end_time - self.start_time)
+            print time_elapsed
+            if time_elapsed > 60:
+                print "*" * 10
+                print "*" * 10
+                print self.count, " tweets in ", time_elapsed, " seconds!"
+                print "*" * 10
+                print "*" * 10
 
     def on_error(self, status):
         error_counter = 0
@@ -93,4 +97,4 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(locations=[-70.93044414,41.65536809,-70.9231071,41.66316909])
+    stream.filter(locations=[-124.848974, 24.396308, -66.885444, 49.384358])
