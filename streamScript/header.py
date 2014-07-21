62dccd721cddec1a506d54a383086ed525077909
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-21 10:40:43 -0700

    deleted old secret keys

--- streamScript/header.py
+++ streamScript/header.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-consumer_key = 'qpKACSg7dvOjjvFJ0xLwToSy0'
-consumer_secret = 'dnFjr3mf7BljHO9riMKE7Aqg9pH5bNeHhrMlwZkUrNAcorOG5C'
-
-access_token = '2584502599-mTHW7xwBXc582UzhDxCP75S1kNP4wFQdb8CbUqL'
-access_token_secret = 'X7xXVvBaNSca3H5ssZ2PrbVmH9jNGFOW0OrzuiiqEGrpD'
-=======
 consumer_key = 'hWMHWIJYoJ4UIG0KNwXcC4pbg'
 consumer_secret = '85E7dAk4ZkJEyNkQ0EbxYvavL7FeKUwEEJlXOs9QnXDwIcWL5c'
 access_token = '249913463-xJhkkoiipEVF0xIJeZc9dys8N1qovmZGmgqiSLaV'
 access_token_secret = 'q4CleTUfctg4BfQz6R5cRpa8EekBylIRzr63fCuargyDa'
->>>>>>> d98984a7b221b7081f2b43f9e1e35c569ccd8090
+
