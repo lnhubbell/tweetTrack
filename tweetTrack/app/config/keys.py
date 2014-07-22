d57f8397bcbae88b6ac5b3705623e77aa61887e5
Ian Auld <imauld@gmail.com>
2014-07-22 14:27:40 -0700

    Removed config files

--- tweetTrack/app/config/keys.py
+++
@@ -1,5 +0,0 @@
-class TwitterKeys(object):
-    consumer_key = 'hWMHWIJYoJ4UIG0KNwXcC4pbg'
-    consumer_secret = '85E7dAk4ZkJEyNkQ0EbxYvavL7FeKUwEEJlXOs9QnXDwIcWL5c'
-    access_key = '249913463-xJhkkoiipEVF0xIJeZc9dys8N1qovmZGmgqiSLaV'
-    access_secret = 'q4CleTUfctg4BfQz6R5cRpa8EekBylIRzr63fCuargyDa'
