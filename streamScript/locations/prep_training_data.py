d6ae7765a1c94b2f2b22ee8c19f6658111f3ad3e
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-22 16:13:49 -0700

    finished merge and removed the pickles which were too big for github

--- streamScript/locations/prep_training_data.py
+++ streamScript/locations/prep_training_data.py
@@ -6,6 +6,9 @@ from sklearn.naive_bayes import MultinomialNB as MNB
 from sklearn.cross_validation import cross_val_score
 import numpy as np
 import cPickle
+from streamScript.send_data import execute_query, _get_connection
+from twitter_keys import my_keys
+
 
 u"""Reads in a file of cities and their bounding boxes. Queries the database
 to get a list of all unique users who have tweeted from that city. Queries Twitter api
@@ -13,23 +16,18 @@ to get 200 tweets from each user, then inserts 200 tweets for up to 100 users pe
 into a separate database table called "Tweet200."""
 
 
-class TwitterKeys(object):
-    consumer_key = 'hWMHWIJYoJ4UIG0KNwXcC4pbg'
-    consumer_secret = '85E7dAk4ZkJEyNkQ0EbxYvavL7FeKUwEEJlXOs9QnXDwIcWL5c'
-    access_key = '249913463-xJhkkoiipEVF0xIJeZc9dys8N1qovmZGmgqiSLaV'
-    access_secret = 'q4CleTUfctg4BfQz6R5cRpa8EekBylIRzr63fCuargyDa'
-
-
 def get_twitter_api():
-    auth = tweepy.OAuthHandler(
-        TwitterKeys.consumer_key,
-        TwitterKeys.consumer_secret
-    )
-    auth.set_access_token(
-        TwitterKeys.access_key,
-        TwitterKeys.access_secret
-    )
-    return tweepy.API(auth)
+    u"""Gets twitter keys from key file."""
+    for our_set, our_keys in my_keys.items():
+        auth = tweepy.OAuthHandler(
+            our_keys['consumer_key'],
+            our_keys['consumer_secret']
+        )
+        auth.set_access_token(
+            our_keys['access_key'],
+            our_keys['access_secret']
+        )
+        yield tweepy.API(auth)
 
 
 def read_in_bb_file():
@@ -50,7 +48,7 @@ def read_in_bb_file():
     return bb_dict
 
 
-def query_db(new_pickle=False):
+def query_all_db(new_pickle=False):
     u"""Calls the file reading function to get in a dict of bounding boxes
     for the 100 most populous US cities. Returns a dict containing all tweets
     collected from each city (with the key being the city name and the value
@@ -59,12 +57,7 @@ def query_db(new_pickle=False):
     bb_dict = read_in_bb_file()
     data_set = {}
     for key, values in bb_dict.items():
-        lats = values[0]
-        longs = values[1]
-        vals = (lats[0], lats[1], longs[0], longs[1])
-        sql = """SELECT * FROM "Tweet" WHERE (location_lat BETWEEN %s AND %s) AND (location_lng BETWEEN %s AND %s) LIMIT 10000; """
-        print "Querying database..." + str(i)
-        data = execute_query(sql, vals)
+        data = query_db(key, values)
         data_set[key] = data
         i += 1
     if new_pickle:
@@ -75,24 +68,8 @@ def query_db(new_pickle=False):
     return data_set
 
 
-def get_unique_handles(data):
-    u"""Takes in a dict with locations as the keys and a list of tweets
-    as the values. Returns a dict of unique user handles for each location."""
-    users_by_city = {}
-    for key, vals in data.items():
-        users = {}
-        for tweet in vals:
-            name = tweet[1]
-            if name in users:
-                users[name] += 1
-            else:
-                users[name] = 1
-        users_by_city[key] = users
-    return users_by_city
-
-
 def build_vocab(data, n=1000):
-    u"""Takes in a dict with locations as the keys and a list of tweets
+    u"""MAY BE DEPRECATED Takes in a dict with locations as the keys and a list of tweets
     as the values. Returns a list of tuples (word, word count) for the
     top n words."""
     vocab = {}
@@ -146,61 +123,131 @@ def build_matrix(data, n=1000):
     return X, Y, vec.get_feature_names()
 
 
+def query_db(city, values):
+    u"""Calls the file reading function to get in a dict of bounding boxes
+    for the 100 most populous US cities. Returns a dict containing all tweets
+    collected from each city (with the key being the city name and the value
+    being a list of tweets)."""
+    lats = values[0]
+    longs = values[1]
+    vals = (lats[0], lats[1], longs[0], longs[1])
+    sql = """SELECT * FROM "Tweet" WHERE
+        (location_lat BETWEEN %s AND %s)
+        AND (location_lng BETWEEN %s AND %s); """
+    print "Querying database for ", city
+    data = execute_query(sql, vals, need_results=True)
+    return data
 
 
-
-def query_twitter_for_histories(data):
-    users_by_city = get_unique_handles(data)
-    api = get_twitter_api()
-    whole_set = {}
-    for city, users in users_by_city.items():
-        city_tweets = []
-        user_count = 0
-        while user_count < 1:
-            for user in users:
-                tweet_his = []
-                history = []
-                try:
-                    history = api.user_timeline(screen_name=user, count=20)
-                except tweepy.error.TweepError as err:
-                    print "got a tweepy error"
-                    print err.message
-                if len(history) >= 20:
-                    user_count += 1
-                    for tweet in history:
-                        screen_name = user
-                        text = tweet.text
-                        created_at = tweet.created_at
-                        location = tweet.geo
-                        if location:
-                            location_lat = location['coordinates'][0]
-                            location_lng = location['coordinates'][1]
-                        hashtags = []
-                        # try:
-                        #     hashtags = [i['text'] for i in tweet.entities.hashtags]
-                        # except AttributeError:
-                        #     print "attribute error"
-                        if location:
-                            blob = (
-                                screen_name, text, location_lat, location_lng,
-                                created_at, hashtags, city
-                            )
-                            tweet_his.append(blob)
-                    if len(tweet_his):
-                        city_tweets.append(tweet_his)
-                        print "added one!"
-                        print city_tweets
-        whole_set[city] = city_tweets
-    return whole_set
+def get_unique_handles(vals):
+    u"""Takes in a dict with locations as the keys and a list of tweets
+    as the values. Returns a dict of unique user handles for each location."""
+    users = {}
+    for tweet in vals:
+        name = tweet[1]
+        if name in users:
+            users[name] += 1
+        else:
+            users[name] = 1
+    heavy_users = []
+    for user in users:
+        if users[user] > 5:
+            heavy_users.append(user)
+    return heavy_users
 
 
-def send_user_queries_to_db(tweet_set):
-    for city, blobs in tweet_set:
-        for blob in blobs:
-            if blob:
-                sql = """INSERT INTO "Tweet200" (screen_name, text, location_lat, location_lng, created_at, hashtags, city) VALUES (%s, %s, %s, %s, %s, %s, %s); """
-                execute_query(sql, blob)
-                print "Sending to database..."
+def format_blob(history, user, city):
+    u"""Formats tweets pieces to be fed to sql query."""
+    tweet_his = []
+    for tweet in history:
+        screen_name = user
+        text = tweet.text
+        created_at = tweet.created_at.strftime('%m/%d/%Y')
+        location = tweet.geo
+        if location:
+            location_lat = location['coordinates'][0]
+            location_lng = location['coordinates'][1]
+        hashtags = []
+        if location:
+            blob = (
+                screen_name, text, location_lat, location_lng,
+                created_at, hashtags, city
+            )
+            tweet_his.append(blob)
+    return tweet_his
+
+
+def query_twitter_for_histories(users, city):
+    u"""Calls function to return a dict of cities and the unique users for each
+    city. Iterates over the dict to extract the tweet text/locations/timestamps
+    for each tweet, bundles results into DB-friendly tuples."""
+    api = get_twitter_api().next()
+    city_tweets = []
+    user_count = 0
+    too_low_count = 0
+    for user in users:
+        if user_count > 100:
+            break
+        history = []
+        try:
+            history = api.user_timeline(screen_name=user, count=200)
+        except tweepy.error.TweepError as err:
+            print "Tweepy Error: ", err.message
+            if err.message == "[{u'message': u'Rate limit exceeded', u'code': 88}]":
+                api = get_twitter_api().next()
+            continue
+        if len(history) >= 200:
+            user_count += 1
+            tweet_his = format_blob(history, user, city)
+        if len(tweet_his):
+            city_tweets.append(tweet_his)
+            print user_count
+        else:
+            print 'not enough tweets in this users history'
+            too_low_count += 1
+        total = user_count + too_low_count
+        print "total requests: ", total
+    return city_tweets
+
+
+def send_user_queries_to_db(tweet_set, city):
+    u"""Sends formatted tweets into DB."""
+    for blob in tweet_set:
+        if blob:
+            for tweet in blob:
+                if tweet:
+                    sql = """INSERT INTO "Tweet200" (screen_name,
+                        text, location_lat, location_lng, created_at,
+                        hashtags, city) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        ; """
+                    execute_query(sql, tweet)
+                    print "Sending to database..."
+    _get_connection().commit()
+    with open('stop_cities.txt', 'a') as fff:
+        fff.write(city)
+        fff.write("\n")
+    print "writing city to stop_cities file"
+    print "committed tweets from ", city, " to DB"
+
+
+def process_each_city():
+    u"""Calls functions to insert user data into Tweet200 table."""
+    bb_dict = read_in_bb_file()
+    for city, values in bb_dict.items():
+        with open("stop_cities.txt", "r") as ffff:
+            stop_cities = ffff.read()
+        if city not in stop_cities:
+            vals = query_db(city, values)
+            print "Now checking ", city
+            handles = get_unique_handles(vals)
+            print city, len(handles)
+            if len(handles) >= 150:
+                print "Now querying twitter for histories"
+                tweets = query_twitter_for_histories(handles, city)
+                if len(tweets) >= 100:
+                    send_user_queries_to_db(tweets, city)
+                else:
+                    print "Not enough users with twitter histories in ", city
 
 
 if __name__ == "__main__":
@@ -245,5 +292,4 @@ if __name__ == "__main__":
     #         nulls += 1
     #         null_keys.append(key)
     # print "No values for ", nulls, " cities: ", null_keys
-
 
