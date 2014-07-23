388484b6f45798e6640d35b5c8730a41587a2007
corinnelhh <corinne.hutchinson@gmail.com>
2014-07-23 10:22:48 -0700

    moved commented print statements

--- streamScript/domain/get_db_report.py
+++ streamScript/domain/get_db_report.py
@@ -20,3 +20,22 @@ def write_report():
 
 if __name__ == "__main__":
     write_report()
+
+
+
+
+    # for city, users in our_outs.items():
+    #     print city, len(users)
+
+    # for city, tweets in our_outs.items:
+    #     print "\n\n", "*" * 10, "\n\n"
+    #     print city, "\n\n"
+    #     print len(tweets)
+    #     print "\n\n", "*" * 20, "\n\n"
+    # nulls = 0
+    # null_keys = []
+    # for key, vals in data.items():
+    #     if len(vals) < 1:
+    #         nulls += 1
+    #         null_keys.append(key)
+    # print "No values for ", nulls, " cities: ", null_keys
