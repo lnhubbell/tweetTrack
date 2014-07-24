29b7f309b05c7f76b283583c29969c76481916dc
Ian Auld <imauld@gmail.com>
2014-07-23 17:23:59 -0700

    Added key activation method

--- streamScript/webAPI/views.py
+++ streamScript/webAPI/views.py
@@ -2,7 +2,7 @@ from re import match
 from random import random
 from flask import jsonify, request, url_for
 from flask.ext.mail import Message
-from streamScript.webAPI import app, mail
+from streamScript.webAPI import app, mail, db
 from streamScript.webAPI.auth.models import APIKey
 from streamScript.webAPI.auth.exceptions import HTTP401
 
@@ -35,14 +35,26 @@ def get_key():
         email = request.get_json().get('email', False)
         if match('^\S+@\S+[\.][0-9a-z]+$', email):
             key = APIKey()
+            db.session.add(key)
+            db.session.commit()
             message = Message(
                 'Activate Your Key',
-                sender=email,
-                recipients=['tweet.track@gmail.com']
+                sender='tweet.track@gmail.com',
+                recipients=[email]
             )
             message.body = """Your tweetTrack API key is  {}.
                 Please visit <a href={}>this link</a> to activate it:
                 """.format(key.key, url_for('activate_key', _external=True))
             mail.send(message)
+    except:
+        pass
 
 
+@app.route('/activate/<key>')
+def activate_key(key):
+    try:
+        check_key = APIKey.query.filter(APIKey.key == key).one()
+        check_key.activate()
+        return '<h1>Your key is now active.</h1>'
+    except NoResultFound:
+        return '<h1>This is not a valid key</h1>'
