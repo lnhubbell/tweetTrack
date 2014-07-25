790028d084d739485b19cd82625681fd97fcf764
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-24 22:16:56 -0700

    moved generate predictions to this file

--- streamScript/domain/make_predictions_per_user.py
+++ streamScript/domain/make_predictions_per_user.py
@@ -31,6 +31,35 @@ def make_prediction(name):
             return user
 
 
+def generate_predictions(userTestdata):
+    u"""Takes in a list of twitter users' last 200 tweets, formatted as
+    'blobs'. Returns a percent correct (if known), a list of all incorrect
+    guesses (or unknown), and a list of all the city predictions."""
+    mnb = picklers.load_pickle('classifier_pickle')
+    vocab = picklers.load_pickle('vocab_pickle')
+    X, user_array, user_cities = build_test_matrix(userTestdata, vocab)
+    correct = 0
+    incorrect = 0
+    got_wrong = []
+    all_results = []
+    predictions = mnb.predict_log_proba(X)
+    y = picklers.load_pickle('labels_pickle')
+    print user_array
+    print user_cities
+    print zip(tuple(y), tuple(predictions))
+    if len(predictions):
+        for idx, prediction in enumerate(predictions):
+            report = (user_array[idx], user_cities[idx], prediction)
+            if user_cities[idx] == prediction:
+                correct += 1
+            else:
+                incorrect += 1
+                got_wrong.append(report)
+            all_results.append(report)
+        percent_right = correct / (float(correct) + incorrect)
+        return percent_right, got_wrong, all_results, user_cities
+
+
 def serve_predictions(names):
     results = make_prediction(names)
     for result in results:
